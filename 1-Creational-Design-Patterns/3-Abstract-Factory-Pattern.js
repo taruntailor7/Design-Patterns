47c89f3226220d83e73de92780396193e998ff86
Tarun Tailor <taruntailor7@gmail.com>
2024-10-20 17:20:26 +0530

    feat: abstract factory pattern added

--- 1-Creational-Design-Patterns/3-Abstract-Factory-Pattern.js
+++ 1-Creational-Design-Patterns/3-Abstract-Factory-Pattern.js
@@ -36,7 +36,6 @@ const modernFactory = new VictorianFurnitureFactory();
 const modernChair = modernFactory.createChair();
 const modernTable = modernFactory.createTable();
 
-
 // What is the Abstract Factory Pattern?
 // The Abstract Factory Pattern is a way of organizing how you create groups of things that are related to each other. It provides a set of rules or instructions that let you create different types of things without knowing exactly what those things are. This helps you keep everything organized and lets you switch between different types easily.
 
